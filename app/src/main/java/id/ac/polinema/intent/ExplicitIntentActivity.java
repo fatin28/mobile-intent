a0c11e4fb9abbed84c7c8155b0245f21b1307e04
fatin28 <fatinfuraida75@gmail.co.id>
2020-02-21 06:28:57 +0700

    Mengerjakan latihan intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +15,13 @@ public class ExplicitIntentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_explicit_intent);
     }
 
+    private EditText nameInput;
+    private TextView outputText;
+
+
+    public void submit(View view) {
+        EditText nameInput = (EditText) findViewById(R.id.input_name);
+        TextView outputText= (TextView) findViewById(R.id.text_output);
+        outputText.setText("Hallo "+ nameInput.getText());
+    }
 }
